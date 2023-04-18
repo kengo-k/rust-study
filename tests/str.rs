de4316360dfa944cd43c3d14f40b7dff4d9711cd
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-04-18 23:55:35 +0900

    str.ts: 文字列を修正する例を追加

--- tests/str.rs
+++ tests/str.rs
@@ -4,7 +4,15 @@ mod tests {
     #[test]
     fn test_print() {
         // 文字列リテラルの型は&strとなる
-        let s: &str = "hello"
-        println!("{}", message);
+        let s: &str = "hello";
+        println!("{}", s);
+
+        // &strは変更できないので文字列を修正する場合はStringを使う
+        let words = vec!["Hello", "World"];
+        let mut result = String::new();
+        for w in words {
+            result.push_str(w);
+        }
+        println!("result string: {}", result)
     }
 }
