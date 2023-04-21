743205c0115a342133a36190d9ec211b5f230ac5
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-04-21 23:59:12 +0900

    struct.rs: ライフタイムパラメータを使った例

--- tests/struct.rs
+++ tests/struct.rs
@@ -7,6 +7,10 @@ mod tests {
         password: String,
     }    
 
+    struct User2<'a> {
+        username: &'a str
+    }
+
     fn create_user(username: String, email: String, password: String) -> User {
         User {
             username: username, 
@@ -18,6 +22,10 @@ mod tests {
     #[test]
     fn test_struct() {
         let user = create_user("user1".to_string(), "email1".to_string(), "password1".to_string());
+        let name2 = "john";
+        let user2 = User2 {
+            username: name2
+        };
         println!("user: {:?}", user);
     }
 }
