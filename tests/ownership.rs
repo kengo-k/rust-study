042f077bf86ed379c260c5637d348ce2dc75933d
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-04-24 23:37:46 +0900

    ownership.rs: &strとStringについて比較

--- tests/ownership.rs
+++ tests/ownership.rs
@@ -3,9 +3,16 @@ mod tests {
 
     #[test]
     fn test_ownership() {
-        let a = String::from("hello");
+        // 文字列リテラルは不変なので代入しても所有権の問題は発生しない
+        let a = "hello";
         let b = a;
+
+        // Stringは可変なので所有権の問題が発生する
+        let x = String::from("world");
+        let y = &x;
         println!("a: {}", a);
         println!("b: {}", b);
+        println!("x: {}", x);
+        println!("y: {}", y);
     }
 }
