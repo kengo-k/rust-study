47745dac6c9035424db9c9f6ed7d37e0e57f7bd5
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-05-04 13:45:08 +0900

    enum.rs: 列挙型サンプルを追加

---
+++ tests/enum.rs
@@ -0,0 +1,32 @@
+#[cfg(test)]
+mod tests {
+
+    // 列挙型の例
+    enum Status {
+        SUCCESS, 
+        // 個々の列挙型の値は独自の値を持つことができる
+        // ここでは単純な数値型やStringを使用しているが構造体や列挙型など任意の値を持てる
+        ERROR(i32, String)
+    }
+
+    #[test]
+    fn test_enum() {
+        let s1 = Status::SUCCESS;
+        let s2 = Status::ERROR(404, "NOT FOUND".to_string());
+
+        fn check_status(s: Status) -> bool {
+            match s {
+                Status::SUCCESS => true,
+                // マッチが不要な変数は_を使用できる
+                Status::ERROR(_, msg) => {
+                    println!("error: {}", msg);
+                    false
+                }
+            }
+        }
+
+        check_status(s1);
+        check_status(s2);
+    }
+
+}
