32792a016275ab91fe232417dc658c212ac01e48
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-08-07 23:53:49 +0900

    update: var.rs

--- tests/var.rs
+++ tests/var.rs
@@ -1,22 +1,59 @@
 #[cfg(test)]
 mod tests {
 
+    #[derive(Debug)]
+    pub struct Person {
+        pub age: i32,
+        pub name: String,
+    }
+
+    impl Person {
+
+        pub fn new(age: i32, name: String) -> Person {
+            Person { age, name }
+        }
+
+        pub fn set(&mut self, age: i32, name: String) {
+            self.age = age;
+            self.name = name;
+        }
+
+    }
+
     #[test]
-    fn test_var() {
-        // letで変数を定義する
+    fn test_var1() {
+        // Declare a variable using 'let'.
         let x = 5;
         println!("1st x: {}", x);
-        
-        // letで定義した変数は変更できないため下記のコードはビルドエラーとなる。
+
+        // Variables declared with 'let' are immutable, so the code below will not compile.
         // x = 10;
-        // 再度letで定義した場合は前の定義を上書きするので問題ない
+
+        // You can declare the same variable name again using 'let'
         let x = 10;
         println!("2nd x: {}", x);
-        
-        // 変更可能な変数を定義するためにはletのあとにmutを指定する
-        let mut y = 20;
+
+        // By declaring a variable with 'mut', you can change its value.
+        let mut y = 10;
         println!("1st y: {}", y);
-        y = 30;
+
+        y = 20;
         println!("2nd y: {}", y);
+    }
+
+    #[test]
+    #[allow(unused_variables)]
+    fn test_var2() {
+        // In the case of a mutable struct, both reassignment to the variable and modification of the struct's values become possible.
+        let mut p1 = Person::new(30, "John".to_string());
+
+        // reassignment
+        p1 = Person::new(35, "Bob".to_string());
+
+        // update struct's values
+        p1.set(32, "Alice".to_string());
+
+        println!("person: {:?}", p1);
+
     }
 }
