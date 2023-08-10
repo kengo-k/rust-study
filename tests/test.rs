8eedc3643663a8408af8dcbb8f47f2ab95894931
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-08-10 21:57:45 +0900

    chore: test.rs

--- tests/test.rs
+++ tests/test.rs
@@ -1,16 +1,12 @@
-// main.rs
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+
+    pub fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
 
     #[test]
     fn test_add() {
-        assert_eq!(add(2, 2), 4);
-        assert_eq!(add(0, 0), 0);
-        assert_eq!(add(-2, 2), 0);
+        assert_eq!(add(1, 2), 3);
     }
 }
